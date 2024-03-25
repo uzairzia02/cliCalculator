821859bbc585fd85c274e664016c4220cd365378
uzairzia <uzair.governorsindh@gmail.com>
2024-03-26 01:35:16 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+let answer = await inquirer.prompt([
+    { message: "Enter first number: ", type: "number", name: "firstnumber" },
+    { message: "Enter second number : ", type: "number", name: "secondnumber" },
+    { message: "Select operation you want to perfom : ", type: "list", name: "operator",
+        choices: ["Addition", "Substraction", "Multiplication", "Division"], },
+]);
+if (answer.operator === "Addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
+else if (answer.operator === "Substraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstnumber / answer.secondnumber);
+}
+else
+    console.log("Please select valid Operator !");
